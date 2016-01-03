8f3e6ed3e0b18ba3d275db27ba7c3fa9b3dc0019
cfarrugia <chris@yobetit.com>
2016-01-03 18:23:49 +0100

    deleting unwanted files

--- src/main/java/com/studylist/StudyListApplication.java
+++ src/main/java/com/studylist/StudyListApplication.java
@@ -1,12 +1,17 @@
 package com.studylist;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyListApplication {
 
+    private static final Logger log = Logger.getLogger(StudyListApplication.class);
+
     public static void main(String[] args) {
+        log.info("**** Starting Application");
         SpringApplication.run(StudyListApplication.class, args);
+        log.info("**** Application Started");
     }
 }
